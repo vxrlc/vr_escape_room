501676fa80314ffa2642529a739043edb7b188f9
mikethevrguy <40547376+mikethevrguy@users.noreply.github.com>
2019-04-03 22:42:59 -0600

    lock work for win logic
    Created some prefab variants, add logic to snap key to lock when it's close. Added logic to allow key to turn in lock. Still outstanding is the logic to unlock the lock. Also may need to revise the lock so the tumbler is separate and can rotate.

---
+++ Assets/lockmanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockmanager : MonoBehaviour
+{
+    public Animator lockOpen;
+    public GameObject key;
+    public BoxCollider trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lockOpen.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (transform.parent.tag == other.tag)
+        {
+            other.gameObject.SetActive(false);
+            key.SetActive(true);
+            trigger.enabled = false;
+        }
+    }
+   public void Unlock()
+    {
+        lockOpen.enabled = true;
+        Invoke("HideLock", 1f);
+    }
+    void HideLock()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
