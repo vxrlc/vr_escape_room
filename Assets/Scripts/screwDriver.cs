3794ac3fe834b20a0661677422ed8f6d5068038d
heatondev <35467354+heatondev@users.noreply.github.com>
2019-04-15 01:15:36 -0600

    Testing sound

--- Assets/Scripts/screwDriver.cs
+++ Assets/Scripts/screwDriver.cs
@@ -13,6 +13,10 @@ public class screwDriver : MonoBehaviour
     public SteamVR_Action_Single actionSqueeze = SteamVR_Input.GetAction<SteamVR_Action_Single>("default", "Squeeze");
     public SteamVR_Action_Vibration haptics;
 
+    public AudioSource drillSound;
+
+    
+
 
     private Quaternion trigSRot;
     private Interactable interactable;
@@ -26,6 +30,7 @@ public class screwDriver : MonoBehaviour
         interactable = GetComponent<Interactable>();
         trigSRot = modelTrigger.localRotation;
         gameManager = FindObjectOfType<GameManager>() as GameManager;
+        drillSound = GetComponent<AudioSource>();
       
     }
     // function to indicate the drill is being held, used to prevent laser pointer from being used
@@ -46,15 +51,16 @@ public class screwDriver : MonoBehaviour
             squeeze = actionSqueeze.GetAxis(hand);
             Vibrate(10, 200, 100, SteamVR_Input_Sources.RightHand);
 
-
+            
         }
+
+      
         modelTrigger.localRotation = trigSRot;
         modelTrigger.Rotate(squeeze * triggerRot, 0, 0, Space.Self);
         gameManager.drillRotation = squeeze * triggerRot;
         gameManager.drillTriggerSqueeze = squeeze;
         drillBit.Rotate(0, 0, squeeze * triggerRot, Space.Self);
-       
-
+      
     }
     private void Vibrate(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
     {
