0fad1fa8109e352015c5b5326745150604a23ddc
heatondev <35467354+heatondev@users.noreply.github.com>
2019-04-17 01:00:43 -0600

    Update screwDriver.cs
    sound working on screwdriver

--- Assets/Scripts/screwDriver.cs
+++ Assets/Scripts/screwDriver.cs
@@ -66,8 +66,10 @@ public class screwDriver : MonoBehaviour
         gameManager.drillTriggerSqueeze = squeeze;
         drillBit.Rotate(0, 0, squeeze * triggerRot, Space.Self);
 
+        // not used  float trigPress = squeeze * triggerRot;
         
         //sets pitch based on the value of the squeeze single
+        drillSound.pitch = Mathf.Lerp(minPitch, maxPitch, (squeeze));
 
         // drill soudn is always playing, sets vol based on value of squeeze
         drillSound.volume = Mathf.Lerp(minVol, maxVol, (squeeze/2));
